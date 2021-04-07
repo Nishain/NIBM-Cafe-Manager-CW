b5d788e5f9fed2d8a52f19588d9d2bd4e1f00d15
Nishain <nishain.atomic@gmail.com>
2021-04-07 13:24:06 +0530

    add new category implemented

--- Cafe-Manager/TableViews/CategoryList.swift
+++ Cafe-Manager/TableViews/CategoryList.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-
+import FirebaseFirestore
 class CategoryList: UITableView, UITableViewDelegate, UITableViewDataSource {
-    var catergories:[String] = ["abc","fnksdfsf","nfjksdfndf","fnjskdnfs","uywierwer"]
+    var catergories:[(id:String,name:String)] = []
+    let db = Firestore.firestore()
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         catergories.count
     }
@@ -18,11 +19,21 @@ class CategoryList: UITableView, UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "catergoryCell")!
         cell.frame = cell.frame.inset(by: UIEdgeInsets(top: 15, left: 10, bottom: 15, right: 10))
         let text = cell.contentView.subviews[0] as! UILabel
-        text.text = catergories[indexPath.row]
+        text.text = catergories[indexPath.row].name
         return cell
+    }
+    func loadCategories(){
+        db.collection("category").getDocuments(completion: {data,err in
+            self.catergories = (data?.documents ?? []).map({
+                (id:$0.documentID,
+                 name:$0.data()["name"] as! String)
+            })
+            self.reloadData()
+        })
     }
     func setDragDelete(index:Int)->[UIContextualAction]{
         let closeAction = UIContextualAction(style: .normal, title:  "Delete", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+            self.db.collection("category").document(self.catergories[index].id).delete()
             self.catergories.remove(at: index)
             self.reloadData()
             success(true)
@@ -36,13 +47,28 @@ class CategoryList: UITableView, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         return UISwipeActionsConfiguration(actions: setDragDelete(index: indexPath.row))
     }
-    @objc func onRemoveCategory(sender:UIButton){
-        catergories.remove(at: sender.tag)
+    
+    func addNewCategory(name:String,alert:AlertPopup){
+        db.collection("category").whereField("name", isEqualTo: name).limit(to: 1).getDocuments(completion: {
+            data,error in
+            if (data?.documents ?? []).count > 0{
+                alert.infoPop(title: "Name already exit!", body: "Category already exist,please choose a different name")
+                return
+            }
+            var ref:DocumentReference?
+            ref = self.db.collection("category").addDocument(data: ["name":name],completion: {err in
+                if err == nil{
+                    self.catergories.append((id:ref!.documentID,name:name))
+                    self.reloadData()
+                }
+            })
+        })
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         delegate = self
         dataSource = self
+        loadCategories()
     }
     /*
     // Only override draw() if you perform custom drawing.
---
+++ Cafe-Manager/ViewControllers/NewCategoryScreen.swift
@@ -0,0 +1,39 @@
+//
+//  NewCategoryScreen.swift
+//  Cafe-Manager
+//
+//  Created by Nishain on 4/7/21.
+//  Copyright © 2021 Nishain. All rights reserved.
+//
+
+import UIKit
+
+class NewCategoryScreen: UIViewController {
+    
+    @IBOutlet weak var categoryNameTxt: UITextField!
+    @IBOutlet weak var categoryList: CategoryList!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onCategoryAdded(_ sender: Any) {
+        let alert = AlertPopup(self)
+        if categoryNameTxt.text?.count == 0{
+            return alert.infoPop(title: "Missing name", body: "Please enter a unique category name")
+        }
+        categoryList.addNewCategory(name: categoryNameTxt.text!,alert:alert)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
